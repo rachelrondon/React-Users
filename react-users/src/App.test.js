daf482f43b9116c778af41c9609b400ff81d5cbb
Rachel Rondon <rachel.sun.rondon@gmail.com>
2019-06-19 07:25:13 -0400

    test updates

--- react-users/src/App.test.js
+++ react-users/src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
+
+configure({adapter: new Adapter()});
 
 it('renders without crashing', () => {
   shallow(<App />);
