a095d795b9c046b5dc5035d0dad70b625020a948
Rachel Rondon <rachel.sun.rondon@gmail.com>
2019-06-18 22:03:37 -0400

    add tests

--- react-users/src/App.test.js
+++ react-users/src/App.test.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('renders name title', () => {
+  const wrapper = shallow(<App />);
+  const name = <div>Name</div>
+  expect(wrapper.contains(name)).toEqual(true);
+});
+
+it('renders name title', () => {
+  const wrapper = shallow(<App />);
+  const email = <div>Email</div>
+  expect(wrapper.contains(email)).toEqual(true);
+});
+
+it('renders name title', () => {
+  const wrapper = shallow(<App />);
+  const city = <div>City</div>
+  expect(wrapper.contains(city)).toEqual(true);
+});
+
+it('renders name title', () => {
+  const wrapper = shallow(<App />);
+  const company = <div>Company</div>
+  expect(wrapper.contains(company)).toEqual(true);
 });
